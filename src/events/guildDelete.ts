f9275966931f97caf2c258f99121ee4c1da60a1d
Ubuntu <oooguuh@gmail.com>
2025-02-15 09:12:12 +0100

    adding new event
    Signed-off-by: Ubuntu <oooguuh@gmail.com>

---
+++ src/events/guildDelete.ts
@@ -0,0 +1,20 @@
+import { Events, Guild } from "discord.js";
+import { PrismaClient } from "@prisma/client";
+
+export const name = Events.GuildDelete;
+
+export async function execute(guild: Guild)
+{
+    const currGuildId = parseInt(guild.id as string);
+    const db = new PrismaClient();
+    await db.guilds.update({
+        where: {
+            guild_id: currGuildId
+        },
+        data: {
+            text_channel_id: 0
+        }
+    })
+    console.log(`onGuildDelete success`);
+    await db.$disconnect();
+}
