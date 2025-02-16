29034c01974900167fe9941083cca8ed8c3e49fd
Ubuntu <oooguuh@gmail.com>
2025-02-16 05:09:08 +0100

    Added setActivity
    Signed-off-by: Ubuntu <oooguuh@gmail.com>

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,4 +1,4 @@
-import { Events, Client } from "discord.js";
+import { Events, Client, ActivityType } from "discord.js";
 
 export const name = Events.ClientReady;
 export const once = true;
@@ -6,4 +6,5 @@ export const once = true;
 export async function execute(client: Client)
 {
     console.log(` Bot is ready logged in as ${client.user!.tag} `);
+    client.user?.setActivity('Loldle', { type: ActivityType.Playing });
 }
