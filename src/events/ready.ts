7bc8c6a96877fa2e72749ab53e348e86e7e42f0c
Ubuntu <oooguuh@gmail.com>
2025-02-16 05:11:15 +0100

    added url attr
    Signed-off-by: Ubuntu <oooguuh@gmail.com>

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -6,5 +6,5 @@ export const once = true;
 export async function execute(client: Client)
 {
     console.log(` Bot is ready logged in as ${client.user!.tag} `);
-    client.user?.setActivity('Loldle', { type: ActivityType.Playing });
+    client.user?.setActivity('Loldle', { type: ActivityType.Playing, url: "https://loldle.net" });
 }
